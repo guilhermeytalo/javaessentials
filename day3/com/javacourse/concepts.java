a9e8d38dad938975557204e9f8a24e18f1b979f5
Guilherme Ytalo <gydaytalo@gmail.com>
2023-07-16 12:19:27 -0300

    day 4
    the name is 3 but just forget

---
+++ day3/com/javacourse/concepts.java
@@ -0,0 +1,24 @@
+package com.javacourse;
+
+public class concepts {
+    public static void main(String[] args) {
+        // Padrão java
+        int age = 20;
+        String name = "";
+        String dogName;
+
+        // aceito mas não utilizado
+        int _age;
+        int $age;
+        String year2014;
+
+        // Não é do java
+        String dog_name;
+        String DogName;
+        String _dogName;
+
+        age = 25;
+
+        System.out.println("age = " + age);
+    }
+}
