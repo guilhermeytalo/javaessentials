0b39874e6d3940b48b43748df4a0d0276a4c546c
Guilherme Ytalo <gydaytalo@gmail.com>
2023-07-17 09:20:17 -0300

    added new patterns

---
+++ day3/com/classmetsimple/CarTesting.java
@@ -0,0 +1,18 @@
+package com.classmetsimple;
+
+public class CarTesting {
+    public static void main(String[] args) {
+            Car fusca2 = new Car();
+            fusca2.brand = "Wolksvagem";
+            fusca2.model = "Fusca";
+            fusca2.passagerNumbers = 4;
+            fusca2.capFuel = 30;
+            fusca2.consumeFuel = 0.15;
+
+            System.out.println(fusca2);
+            System.out.println(fusca2.brand);
+            System.out.println(fusca2.model);
+            fusca2.showAutonomy();
+        }
+
+}
